4cfd978a0355f2d12c32109b1361fcf8a18b2403
Osazee Isere <oz1.concepts@gmail.com>
2021-07-28 11:40:34 +0100

    initial commit

--- LinqUsingEnumerable/Program.cs
+++ LinqUsingEnumerable/Program.cs
@@ -10,6 +10,36 @@ namespace LinqUsingEnumerable
     {
         static void Main(string[] args)
         {
+            QueryStriingOperators();
+            QueryStringsWithEnumerableAndLamda();
+
+
+
         }
+
+        private static void QueryStringsWithEnumerableAndLamda()
+        {
+            Console.WriteLine("Using Enumerable/Lamda expressions");
+            string[] jwVideos = { "Lot Wife 2", "Hezekiah", "Real Love 1", "Josuah" };
+            var subset = jwVideos.Where(vd => vd.Contains(" ")).OrderByDescending(vd => vd).Select(vd => vd);
+            //print the subset
+            foreach (var item in subset)
+            {
+                Console.WriteLine("Item: {0}", item);
+            }
+        }
+
+        private static void QueryStriingOperators()
+        {
+            Console.WriteLine("Using Query Operators");
+            string[] jwVideos = { "Lot Wife 2", "Hezekiah", "Real Love 1", "Josuah" };
+            var subset = from vd in jwVideos where vd.Contains(" ") orderby vd descending select vd;
+            //print the subset
+            foreach(var item in subset)
+            {
+                Console.WriteLine("Item: {0}",item);
+            }
+        }
+
     }
 }
